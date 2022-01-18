c5be0e2d7f2a58b05e3ee2a5a0aed7d8704d62c1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-01-18 11:17:23 +0000

    Add code for A350799

---
+++ A350799.py
@@ -0,0 +1,42 @@
+# arctan = sum_n=0^inf (-1)^n x^(2n+1) / (2n + 1)
+# pi = 16 arctan(1/5) - 4 arctan(1/239)
+
+from fractions import Fraction
+with open("pi.txt") as f:
+    pi = f.read()
+
+
+def compute_pi(n, m):
+    pi = 0
+    for i in range(n):
+        pi += 16 * (-1) ** i * Fraction(1, (2 * i + 1) * 5 ** (2 * i + 1))
+    for i in range(m):
+        pi -= 4 * (-1) ** i * Fraction(1, (2 * i + 1) * 239 ** (2 * i + 1))
+    return pi
+
+
+def get_decimal(n, places=10000):
+    N = int(float(n) // 1)
+    out = f"{N}."
+    n -= N
+    for i in range(places):
+        n *= 10
+        N = int(float(n) // 1)
+        out += f"{N}"
+        n -= N
+    return out
+
+
+def get_ndigits(approx):
+    if not approx.startswith("3."):
+        return -1
+    for ndigits, (i, j) in enumerate(zip(pi[2:], approx[2:])):
+        if i != j:
+            break
+    return ndigits
+
+
+for n in range(1, 201):
+    pi2 = get_decimal(compute_pi(n, n), 1000)
+    ndigits = get_ndigits(pi2)
+    print(n, ndigits)
